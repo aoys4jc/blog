8f0ba75b28e2f090a3967ddc8935e65d4e80a4a3
jiecong <jiecong@outlook.com>
2018-01-24 10:49:26 +0800

    .

--- src/Blog.WebBack/Program.cs
+++ src/Blog.WebBack/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,7 +14,6 @@ namespace Blog.WebBack
     {
         public static void Main(string[] args)
         {
-            Console.Title = "WebBack";
             BuildWebHost(args).Run();
         }
 
