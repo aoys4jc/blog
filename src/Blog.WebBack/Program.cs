f05151ad1dc6004f5e8f060fb089b895c7415e62
jiecong <jiecong@outlook.com>
2018-01-24 10:46:43 +0800

    .

--- src/Blog.WebBack/Program.cs
+++ src/Blog.WebBack/Program.cs
@@ -14,6 +14,7 @@ namespace Blog.WebBack
     {
         public static void Main(string[] args)
         {
+            Console.Title = "WebBack";
             BuildWebHost(args).Run();
         }
 
--- src/Blog.WebMobile/Program.cs
+++ src/Blog.WebMobile/Program.cs
@@ -14,6 +14,7 @@ namespace Blog.WebMobile
     {
         public static void Main(string[] args)
         {
+            Console.Title = "WebMobile";
             BuildWebHost(args).Run();
         }
 
