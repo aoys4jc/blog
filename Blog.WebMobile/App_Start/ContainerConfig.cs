d6612df3fa1ea1906fc9fc227ee11db1f50dc72b
aoys4jc <755606093@qq.com>
2019-01-13 14:26:39 +0800

    升级

---
+++ Blog.WebMobile/App_Start/ContainerConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.WebMobile
+{
+    public static class ContainerConfig
+    {
+        public static void RegisterType(this IServiceCollection services)
+        {
+        }
+    }
+}
---
+++ Blog.WebMobile/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Common;
+using Blog.WebMobile.Models;
+using Exceptionless;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+namespace Blog.WebMobile
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IOptions<AppSetting> AppSettings { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+            services.AddResponseCompression();
+
+            services.AddMvc();
+            services.AddOptions();
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+
+            services.Configure<AppSetting>(Configuration.GetSection("AppString"));
+
+            //注册需要注入的类型
+            services.RegisterType();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddNLog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               
+                app.UseWelcomePage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            env.ConfigureNLog("nlog.config");
+            //app.UseIdentityServer();
+
+            app.UseCors("AllowAll");
+            app.UseStaticFiles();
+            app.UseExceptionless("XDg76an3h3W59kpo2ZLRCoZ3t7kUiafsnbAQCjyn");
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
